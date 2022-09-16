ceb00934965067754a13361d032d07f25aa63c69
Quentin Creuzet <quentin@f451.faith>
2022-09-16 10:37:26 +0200

    Correction of the exercice

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,36 @@ const helloWorld = () => {
 };
 
 helloWorld();
+
+/* CORRECTION */
+
+const clickEventOnListItemHeader = () => {
+  document.querySelectorAll(".list__item__header").forEach((item) => {
+    const body = item.nextElementSibling;
+    item.addEventListener("click", () => {
+      if (body.classList.contains("show")) {
+        hideAllListItemBody();
+      } else {
+        hideAllListItemBody();
+        body.classList.add("show");
+      }
+    });
+  });
+};
+
+const hideAllListItemBody = () => {
+  document.querySelectorAll(".list__item__body").forEach((item) => {
+    item.classList.remove("show");
+  });
+};
+
+const replaceTextByDateOfBirth = () => {
+  document.querySelectorAll(".list__item__artist").forEach((item) => {
+    const text = item.innerText;
+    const dateOfBirth = text.slice(-5, -1);
+    item.innerText = dateOfBirth;
+  });
+};
+
+clickEventOnListItemHeader();
+replaceTextByDateOfBirth();
